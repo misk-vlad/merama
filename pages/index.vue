10e6f69b85084f181a7e59c8acf4e75e993dceac
misk.vlad <misk.vlad@whatisemail.wtf>
2021-10-18 23:00:20 +0300

    feat: added BalmUI menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ui-top-app-bar
+      content-selector=""
+      title="Мерама"
+    >
+      <template #toolbar>
+        <ui-icon-button
+          icon="file_download"
+        ></ui-icon-button>
+        <ui-icon-button icon="print"></ui-icon-button>
+        <ui-icon-button
+          icon="bookmark"
+        ></ui-icon-button>
+      </template>
+    </ui-top-app-bar>
+  </div>
 </template>
 
 <script lang="ts">
