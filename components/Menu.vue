a927f6ec25348e56ded0b1c76aaaa11c10d0e751
misk.vlad <misk.vlad@whatisemail.wtf>
2021-10-19 22:58:54 +0300

    chore: added layout.vue

---
+++ components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <ui-top-app-bar
+    title="Мерама"
+    content-selector=""
+    type="1"
+  >
+
+    <template #toolbar="{ toolbarItemClass }">
+        <ui-icon-button
+        icon="file_download"
+        :class="toolbarItemClass"
+        ></ui-icon-button>
+        <ui-icon-button 
+        icon="print"
+        :class="toolbarItemClass"
+        ></ui-icon-button>
+        <ui-icon-button
+        icon="bookmark"
+        :class="toolbarItemClass"
+        ></ui-icon-button>
+    </template>
+
+    <template #nav-icon>
+      <ui-icon size="48">storm</ui-icon>
+    </template>
+
+  </ui-top-app-bar>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
-    <ui-top-app-bar
-      content-selector=""
-      title="Мерама"
-    >
-      <template #toolbar>
-        <ui-icon-button
-          icon="file_download"
-        ></ui-icon-button>
-        <ui-icon-button icon="print"></ui-icon-button>
-        <ui-icon-button
-          icon="bookmark"
-        ></ui-icon-button>
-      </template>
-    </ui-top-app-bar>
+    <h1>Index 1</h1>
+    <h1>Index 2</h1>
+    <h1>Index 3</h1>
+    <h1>Index 4</h1>
+    <h1>Index 5</h1>
+    <h1>Index 6</h1>
+    <h1>Index 7</h1>
+    <h1>Index 8</h1>
+    <h1>Index 9</h1>
   </div>
 </template>
 
